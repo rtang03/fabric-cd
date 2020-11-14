bbace47416cbeb5cc007a2952b9570212704f57b
tangross <7686266+rtang03@users.noreply.github.com>
2020-11-15 00:39:49 +0800

    app-of-app org1

--- scripts/bs.argo.org1.sh
+++ scripts/bs.argo.org1.sh
@@ -6,7 +6,7 @@
 SECONDS=0
 TARGET=dev-0.1
 
-helm template ../argo-app --set ns=argocd,path=app-of-app,target=dev-0.1,rel=apps-org1,file=values-org1.yaml
+helm template ../argo-app --set ns=argocd,path=app-of-app,target=dev-0.1,rel=apps-org1,file=values-org1.yaml | argocd app create -f -
 
 duration=$SECONDS
 printf "${GREEN}$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed.\n\n${NC}"
