0b0436bc2465968c58f7792c84649bf1e20cff7b
tangross <7686266+rtang03@users.noreply.github.com>
2020-11-15 01:40:45 +0800

    debug

--- scripts/bs.argo.org1.sh
+++ scripts/bs.argo.org1.sh
@@ -16,7 +16,9 @@ argocd app sync $REL_ORGADMIN1
 
 argocd app wait $REL_ORGADMIN1 --timeout 120
 
-helm template ../argo-app --set ns=argocd,path=argo-wf,target=dev-0.1,rel=argo-org1,file=values-org1.yaml | argocd app create -f -
+helm template ../argo-app --set ns=default,path=argo-wf,target=dev-0.1,rel=argo-cluster,file=values-cluster.yaml,clusterscope=true | argocd app create -
+
+helm template ../argo-app --set ns=n1,path=argo-wf,target=dev-0.1,rel=argo-org1,file=values-org1.yaml | argocd app create -f -
 
 duration=$SECONDS
 printf "${GREEN}$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed.\n\n${NC}"
