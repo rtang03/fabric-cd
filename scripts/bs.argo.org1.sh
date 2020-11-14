062e3fe47aa81af182fff1ebf80af7f35cdaca08
tangross <7686266+rtang03@users.noreply.github.com>
2020-11-15 01:16:59 +0800

    remove version 3

--- scripts/bs.argo.org1.sh
+++ scripts/bs.argo.org1.sh
@@ -8,5 +8,13 @@ TARGET=dev-0.1
 
 helm template ../argo-app --set ns=argocd,path=app-of-app,target=dev-0.1,rel=apps-org1,file=values-org1.yaml | argocd app create -f -
 
+argocd app sync apps-org1
+
+argocd app wait apps-org1 --timeout 120
+
+argocd app sync $REL_ORGADMIN1
+
+argocd app wait $REL_ORGADMIN1 --timeout 120
+
 duration=$SECONDS
 printf "${GREEN}$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed.\n\n${NC}"
