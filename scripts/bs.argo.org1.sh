7f9f9ebfcbd390529b140630ebb883b4d75231e2
tangross <7686266+rtang03@users.noreply.github.com>
2020-11-15 12:09:15 +0800

    clusterWorkflowtmpl removed

--- scripts/bs.argo.org1.sh
+++ scripts/bs.argo.org1.sh
@@ -16,7 +16,7 @@ argocd app sync $REL_ORGADMIN1
 
 argocd app wait $REL_ORGADMIN1 --timeout 120
 
-helm template ../argo-app --set path=argo-wf,target=dev-0.1,rel=argo-cluster,file=values-cluster.yaml,clusterscope=true | argocd app create -
+# helm template ../argo-app --set ns=default,path=argo-wf,target=dev-0.1,rel=argo-cluster,file=values-cluster.yaml,clusterscope=true | argocd app create -
 
 helm template ../argo-app --set ns=n1,path=argo-wf,target=dev-0.1,rel=argo-org1,file=values-org1.yaml | argocd app create -f -
 
