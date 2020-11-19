f316f3375662c98b0514380e791734517e0b60b5
tangross <7686266+rtang03@users.noreply.github.com>
2020-11-19 22:22:07 +0800

    org1 e2e workflow exec

--- scripts/uninstall.argo.sh
+++ scripts/uninstall.argo.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
+set -x
 argocd app list -o name | while read APP
 do
-  set -x
   argocd app delete $APP
-  set +x
 done
 
 argo -n n0 list -o name | while read APP
 do
-  set -x
   argo -n n0 delete $APP
-  set +x
 done
 
 argo -n n1 list -o name | while read APP
 do
-  set -x
   argo -n n1 delete $APP
-  set +x
 done
 
 argo -n n2 list -o name | while read APP
 do
-  set -x
   argo -n n2 delete $APP
-  set +x
 done
